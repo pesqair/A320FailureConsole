50cc99705894a27b4c2fc3137aa10015a682b536
Rafael Pesquera <rafael@pesquera.com>
2017-11-18 11:12:39 -0600

    Add project files.

---
+++ A320FailureConsole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A320FailureConsole.Startup))]
+namespace A320FailureConsole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
