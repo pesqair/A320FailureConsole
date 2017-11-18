bbffc875a1b4d62d6cf9be4e182a1d3f438863fe
Rafael Pesquera <rafael@pesquera.com>
2017-11-18 16:46:46 -0600

    Added ACSystem and Failure classes

---
+++ A320FailureConsole/Models/ACSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A320FailureConsole.Models
+{
+    public class ACSystem
+    {
+        public int ACSystemID { get; set; }
+        [Required()]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required()]
+        public string DREF0prefix { get; set; }
+        public virtual List<Failure> Failures { get; set; }
+    }
+}
---
+++ A320FailureConsole/Models/Failure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace A320FailureConsole.Models
+{
+    public class Failure
+    {
+        public int FailureID { get; set; }
+        [Required()]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ACSystemID { get; set; }
+        public virtual ACSystem ParentSystem { get; set; }
+        [Required()]
+        public string DREF0suffix { get; set; }
+        public float FailValue { get; set; }
+        public float FixValue { get; set; }
+    }
+}
